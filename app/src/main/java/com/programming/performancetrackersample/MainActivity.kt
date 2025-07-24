ce14eec58ae580801a39c27580d331d89c67343f
Jure Lokovsek <lokovsek.jure@gmail.com>
2025-07-24 19:31:12 +0200

    Updated MainActivity.

--- app/src/main/java/com/programming/performancetrackersample/MainActivity.kt
+++ app/src/main/java/com/programming/performancetrackersample/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
+import com.programming.nativelogger.NativeLogger.d
 import com.programming.performancetracker.PerformanceTracker
+import com.programming.performancetrackersample.MainActivity.Companion.LIFECYCLE_LABEL
 import com.programming.performancetrackersample.ui.theme.PerformanceTrackerSampleTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -21,42 +35,100 @@ class MainActivity : ComponentActivity() {
 
     private val performanceTracker = PerformanceTracker
 
+    companion object {
+        const val LIFECYCLE_LABEL = "lifecycle_label"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        performanceTracker.startTracking(label = LIFECYCLE_LABEL)
 
         lifecycleScope.launch {
             performanceTracker.measure(label = "test") {
                 delay(1200)
             }
-            performanceTracker.logAllDurations()
+            d { "WHAT Duration: ${performanceTracker.getDurationForLabelOrNull("test")}" }
         }
 
         setContent {
             PerformanceTrackerSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainView(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = Color.LightGray)
+                            .padding(innerPadding),
+                        onLifecycleClick = {
+                            performanceTracker.getDurationForLabelOrNull(label = LIFECYCLE_LABEL)
+                        }
                     )
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        performanceTracker.stopTracking(label = LIFECYCLE_LABEL)
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainView(
+    modifier: Modifier = Modifier,
+    onLifecycleClick: () -> Unit
+) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        TextButton(
+            text = LIFECYCLE_LABEL.uppercase(),
+            onClick = onLifecycleClick
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainViewPreview() {
+    PerformanceTrackerSampleTheme {
+        MainView(
+            modifier = Modifier
+                .fillMaxSize(),
+            onLifecycleClick = {}
+        )
+    }
+}
+
+@Composable
+fun TextButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit
+) {
     Text(
-        text = "Hello $name!",
+        text = text,
+        style = TextStyle(
+            color = Color.White,
+            fontSize = 16.sp,
+            fontFamily = FontFamily.Monospace,
+            fontWeight = FontWeight.W800,
+            fontStyle = FontStyle.Normal,
+            background = Color.LightGray
+        ),
         modifier = modifier
+            .padding(15.dp)
+            .clickable(onClick = onClick, role = Role.Button)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun TextButtonPreview() {
     PerformanceTrackerSampleTheme {
-        Greeting("Android")
+        TextButton(
+            text = "Text Button",
+            onClick = {}
+        )
     }
 }
